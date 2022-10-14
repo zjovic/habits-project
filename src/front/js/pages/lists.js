cc1f0c314dba2df005e102edf201291a22047aaf
zjovic <38418292+zjovic@users.noreply.github.com>
2022-10-14 17:36:26 +0200

    Display lists

--- src/front/js/pages/lists.js
+++ src/front/js/pages/lists.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
+import { Message } from "../component/message.js";
 
 export const Lists = () => {
   const { store, actions } = useContext(Context);
   const params = useParams();
 
   const [date, setDate] = useState("");
+  const [activeTab, setActiveTab] = useState("todos");
 
   useEffect(() => {
     const getCurrentDate = async () => {
@@ -21,17 +23,50 @@ export const Lists = () => {
     getCurrentDate();
   }, []);
 
+  const handleTodosTab = () => {
+    setActiveTab("todos");
+  };
+
+  const handleHabitsTab = () => {
+    setActiveTab("habits");
+  };
+
+  // if no todos and no habits, fetch
+
   return (
     <div className="lists-container">
       <div className="lists-header">
         <span className="lists-date">{date}</span>
+        <Message />
       </div>
-      <div className="lists-tabs"></div>
-      <ul className="lists-list">
+      <div className="list-tabs">
+        <span
+          className={`list-tab ${activeTab === "todos" ? "active" : ""}`}
+          onClick={handleTodosTab}
+        >
+          todos
+        </span>
+        <span
+          className={`list-tab ${activeTab === "habits" ? "active" : ""}`}
+          onClick={handleHabitsTab}
+        >
+          habits
+        </span>
+      </div>
+      <ul className={`lists-list ${activeTab === "todos" ? "active" : ""}`}>
         {store.todos.map((todo, index) => {
           return (
             <li className="row" key={index}>
               {todo.name}
+            </li>
+          );
+        })}
+      </ul>
+      <ul className={`lists-list ${activeTab === "habits" ? "active" : ""}`}>
+        {store.habits.map((habit, index) => {
+          return (
+            <li className="row" key={index}>
+              {habit.name}
             </li>
           );
         })}
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -3,6 +3,14 @@ const getState = ({ getStore, getActions, setStore }) => {
     store: {
       token: null,
       todos: [],
+      habits: [],
+      messages: [
+        "Either you run the day or the day runs you.",
+        "Setting goals is the first step in turning the invisible into the visible.",
+        "Drop by drop is the water pot filled.",
+        "Good habits are worth being fanatical about.",
+        "Habits change into character.",
+      ],
     },
     actions: {
       setToken: (token) => {
@@ -29,6 +37,28 @@ const getState = ({ getStore, getActions, setStore }) => {
           setStore({ todos: currTodos });
         } catch (error) {
           console.log("Error loading todos from backend", error);
+        }
+      },
+      fetchHabits: async () => {
+        try {
+          const options = {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+            },
+          };
+          const response = await fetch(
+            "http://127.0.0.1:3001/api/habits",
+            options
+          );
+          const data = await response.json();
+
+          const store = getStore();
+          const currHabits = [...store.habits, ...data.habits];
+
+          setStore({ habits: currHabits });
+        } catch (error) {
+          console.log("Error loading habits from backend", error);
         }
       },
     },
